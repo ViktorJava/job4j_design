46be887b44218d2b9388d03b86b1bffe930eafca
ViktorJava <gipsyscrew@gmail.com>
2021-01-04 20:43:54 +0200

    Что такое итератор. [#438890] #6

---
+++ chapter_001/src/main/java/ru/job4j/it/ArrayIt.java
@@ -0,0 +1,47 @@
+package ru.job4j.it;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * <h2>Что такое итератор. [#438890]</h2>
+ * Итератор для одномерного массива чисел. Итератор отдаёт элементы
+ * в обратном порядке.
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 03.01.2021
+ */
+public class ArrayIt implements Iterator<Integer> {
+    private final int[] data;
+    private int point = 0;
+
+    public ArrayIt(int[] data) {
+        this.data = data;
+        this.point = data.length - 1;
+    }
+
+    /**
+     * Метод проверяет, если ли следующий элемент.
+     *
+     * @return true в случае наличия следующего элемента, иначе false.
+     */
+    @Override
+    public boolean hasNext() {
+        return point >= 0;
+    }
+
+    /**
+     * Метод сдвигает указатель итератора в обратном порядке
+     * и возвращает значение.
+     *
+     * @return Элемент ячейки.
+     */
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return data[point--];
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/it/ArrayItTest.java
@@ -0,0 +1,41 @@
+package ru.job4j.it;
+
+import org.junit.Test;
+
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+/**
+ * Тесты итератора одномерного массива, отдающего элементы в обратном порядке.
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 03.01.2021
+ */
+public class ArrayItTest {
+    @Test
+    public void whenMultiCallhasNextThenTrue() {
+        ArrayIt it = new ArrayIt(new int[]{1, 2, 3});
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.hasNext(), is(true));
+    }
+
+    @Test
+    public void whenReadSequence() {
+        ArrayIt it = new ArrayIt(new int[]{1, 2, 3});
+        assertThat(it.next(), is(3));
+        assertThat(it.next(), is(2));
+        assertThat(it.next(), is(1));
+    }
+
+    /**
+     * Тест валидации итератора.
+     */
+    @Test(expected = NoSuchElementException.class)
+    public void whenNextFromEmpty() {
+        ArrayIt it = new ArrayIt(new int[]{});
+        it.next();
+    }
+}
