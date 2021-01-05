5869ba43a72103d03124cd221bac8974a5f90391
ViktorJava <gipsyscrew@gmail.com>
2021-01-05 21:24:29 +0200

    5.1.1. Итератор для двухмерного массива int[][] [#438891] #8

---
+++ chapter_001/src/main/java/ru/job4j/it/MatrixIt.java
@@ -0,0 +1,56 @@
+package ru.job4j.it;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * <h2>Итератор для двухмерного массива int[][] [#438891]</h2>
+ * Написать для двумерного массива, итератор, который
+ * последовательно вернет элементы.
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 04.01.2021
+ */
+public class MatrixIt implements Iterator<Integer> {
+    private final int[][] data;
+    private int col = 0;
+    private int row = 0;
+
+    public MatrixIt(int[][] data) {
+        this.data = data;
+    }
+
+    /**
+     * Метод проверяет, есть ли следующий элемент.
+     *
+     * @return true в случае наличия следующего элемента, иначе false.
+     */
+    @Override
+    public boolean hasNext() {
+        while (row < data.length && data[row].length == 0) {
+            row++;
+        }
+        return (row < data.length && col < data[row].length);
+    }
+
+    /**
+     * Метод смещает указатель и возвращает следующий элемент
+     * двумерного массива.
+     *
+     * @return Следующий элемент двумерного массива.
+     */
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        int result = data[row][col++];
+
+        while (row < data.length && col >= data[row].length) {
+            row++;
+            col = 0;
+        }
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/it/MatrixItTest.java
@@ -0,0 +1,102 @@
+package ru.job4j.it;
+
+import org.junit.Test;
+
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Тесты итератора двумерного массива.
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 04.01.2021
+ */
+public class MatrixItTest {
+    @Test
+    public void when4El() {
+        int[][] in = {
+                {1}
+        };
+        MatrixIt it = new MatrixIt(in);
+        assertThat(it.next(), is(1));
+    }
+
+    @Test
+    public void whenFirstEmptyThenNext() {
+        int[][] in = {
+                {}, {1}
+        };
+        MatrixIt it = new MatrixIt(in);
+        assertThat(it.next(), is(1));
+    }
+
+    @Test
+    public void whenFirstEmptyThenHashNext() {
+        int[][] in = {
+                {}, {1}
+        };
+        MatrixIt it = new MatrixIt(in);
+        assertThat(it.hasNext(), is(true));
+    }
+
+    @Test
+    public void whenRowHasDiffSize() {
+        int[][] in = {
+                {1}, {2, 3}
+        };
+        MatrixIt it = new MatrixIt(in);
+        assertThat(it.next(), is(1));
+        assertThat(it.next(), is(2));
+        assertThat(it.next(), is(3));
+    }
+
+    @Test
+    public void whenFewEmpty() {
+        int[][] in = {
+                {1}, {}, {}, {}, {2}
+        };
+        MatrixIt it = new MatrixIt(in);
+        assertThat(it.next(), is(1));
+        assertThat(it.next(), is(2));
+    }
+
+    @Test
+    public void whenEmpty() {
+        int[][] in = {
+                {}, {}, {}, {}
+        };
+        MatrixIt it = new MatrixIt(in);
+        assertThat(it.hasNext(), is(false));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenEmptyThenNext() {
+        int[][] in = {
+                {}, {}, {}, {}
+        };
+        MatrixIt it = new MatrixIt(in);
+        it.next();
+    }
+
+    @Test
+    public void whenMultiHashNext() {
+        int[][] in = {
+                {}, {1}
+        };
+        MatrixIt it = new MatrixIt(in);
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.hasNext(), is(true));
+    }
+
+    @Test
+    public void whenNoElements() {
+        int[][] in = {
+                {}, {}, {}
+        };
+        MatrixIt it = new MatrixIt(in);
+        assertThat(it.hasNext(), is(false));
+    }
+}
