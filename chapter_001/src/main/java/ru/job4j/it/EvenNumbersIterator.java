8592a701a5f12274eba1e19ed7b362cb02aafa4d
ViktorJava <gipsyscrew@gmail.com>
2021-01-06 21:58:04 +0200

    5.1.2. Создать итератор четные числа [#438888] #9

---
+++ chapter_001/src/main/java/ru/job4j/it/EvenNumbersIterator.java
@@ -0,0 +1,62 @@
+package ru.job4j.it;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * <h2>5.1.2. Создать итератор четные числа [#438888]</h2>
+ * Создать итератор возвращающий только четные цифры.
+ * Итератор должен принимать список произвольных чисел.
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 05.01.2021
+ */
+public class EvenNumbersIterator implements Iterator<Integer> {
+    private final int[] data;
+    private int pointer = -1;
+    private int nextPointer = -1;
+
+    public EvenNumbersIterator(int[] data) {
+        this.data = data;
+    }
+
+    /**
+     * Метод, проверяет, есть ли следующий чётный элемент.
+     *
+     * @return true в случае наличия следующего элемента, иначе false.
+     */
+    @Override
+    public boolean hasNext() {
+        return pointer != findIndex();
+    }
+
+    /**
+     * Метод, смещает указатель и возвращает следующий чётный элемент.
+     *
+     * @return Следующий чётный элемент.
+     */
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        pointer = nextPointer;
+        return data[pointer];
+    }
+
+    /**
+     * Поиск чётного элемента в массиве данных, целочисленного типа.
+     *
+     * @return Индекс нового чётного элемента в случае его наличия.
+     */
+    public int findIndex() {
+        for (int i = pointer + 1; i < data.length; i++) {
+            if (data[i] % 2 == 0) {
+                nextPointer = i;
+                break;
+            }
+        }
+        return nextPointer;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/it/EvenNumbersIteratorTest.java
@@ -0,0 +1,66 @@
+package ru.job4j.it;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Тесты итератора чётных чисел.
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 05.01.2021
+ */
+public class EvenNumbersIteratorTest {
+    private Iterator<Integer> it;
+
+    @Before
+    public void setUp() {
+        it = new EvenNumbersIterator(new int[]{1, 2, 3, 4, 5, 6, 7});
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void shouldReturnEvenNumbersSequentially() {
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(2));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(4));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(6));
+        assertThat(it.hasNext(), is(false));
+        it.next();
+    }
+
+    @Test
+    public void sequentialHasNextInvocationDoesntAffectRetrievalOrder() {
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(2));
+        assertThat(it.next(), is(4));
+        assertThat(it.next(), is(6));
+    }
+
+    @Test
+    public void shouldReturnFalseIfNoAnyEvenNumbers() {
+        it = new EvenNumbersIterator(new int[]{1});
+        assertThat(it.hasNext(), is(false));
+    }
+
+    @Test
+    public void allNumbersAreEven() {
+        it = new EvenNumbersIterator(new int[]{2, 4, 6, 8});
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(2));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(4));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(6));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(8));
+    }
+}
