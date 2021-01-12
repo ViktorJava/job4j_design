afef4f2d7bf05de79fb52ad1e256489f7da8e765
ViktorJava <gipsyscrew@gmail.com>
2021-01-12 23:41:44 +0200

    5.1.4. FlatMap для Iterator<Iterator> [#438889] #10

---
+++ chapter_001/src/main/java/ru/job4j/iterator/FlatMap.java
@@ -0,0 +1,58 @@
+package ru.job4j.iterator;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+/**
+ * <h2>FlatMap для Iterator<Iterator> [#438889]</h2>
+ * В Stream API есть метод flatMap.
+ * Он позволяет получить из элемента потока другой поток.
+ * Необходимо создать подобное поведение.
+ * Класс FlatMap принимает объект вложенных итераторов.
+ * В классе нужно реализовать два метода: {@code next()} и {@code hasNext()}.
+ * Метод {@code next()} должен последовательно вернуть
+ * числа из вложенных итераторов.
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 12.01.2021
+ */
+public class FlatMap<T> implements Iterator<T> {
+    private final Iterator<Iterator<T>> data;
+    private Iterator<T> cursor;
+
+    @Override
+    public boolean hasNext() {
+        while (!cursor.hasNext() && data.hasNext()) {
+            this.cursor = data.next();
+        }
+        return cursor.hasNext();
+    }
+
+    @Override
+    public T next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return cursor.next();
+    }
+
+    public FlatMap(Iterator<Iterator<T>> data) {
+        this.data = data;
+        this.cursor = Collections.emptyIterator();
+    }
+
+    public static void main(String... args) {
+        Iterator<Iterator<Integer>> data = List.of(
+                List.of(1, 2, 3).iterator(),
+                List.of(4, 5, 6).iterator(),
+                List.of(7, 8, 9).iterator()
+        ).iterator();
+        FlatMap<Integer> flat = new FlatMap<>(data);
+        while (flat.hasNext()) {
+            System.out.print(flat.next() + " ");
+        }
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/iterator/FlatMapTest.java
@@ -0,0 +1,71 @@
+package ru.job4j.iterator;
+
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+/**
+ * Тесты реализации FlatMap.
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 12.01.2021
+ */
+public class FlatMapTest {
+    @Test
+    public void whenDiffNext() {
+        Iterator<Iterator<Integer>> data = List.of(
+                List.of(1).iterator(),
+                List.of(2, 3).iterator()
+        ).iterator();
+        FlatMap<Integer> flat = new FlatMap<>(data);
+        assertThat(flat.next(), is(1));
+        assertThat(flat.next(), is(2));
+        assertThat(flat.next(), is(3));
+    }
+
+    @Test
+    public void whenSeqNext() {
+        Iterator<Iterator<Integer>> data = List.of(
+                List.of(1, 2, 3).iterator()
+        ).iterator();
+        FlatMap<Integer> flat = new FlatMap<>(data);
+        assertThat(flat.next(), is(1));
+        assertThat(flat.next(), is(2));
+        assertThat(flat.next(), is(3));
+    }
+
+    @Test
+    public void whenMultiHasNext() {
+        Iterator<Iterator<Integer>> data = List.of(
+                List.of(1).iterator()
+        ).iterator();
+        FlatMap<Integer> flat = new FlatMap<>(data);
+        assertThat(flat.hasNext(), is(true));
+        assertThat(flat.hasNext(), is(true));
+    }
+
+    @Test
+    public void whenHasNextFalse() {
+        Iterator<Iterator<Integer>> data = List.of(
+                List.of(1).iterator()
+        ).iterator();
+        FlatMap<Integer> flat = new FlatMap<>(data);
+        assertThat(flat.next(), is(1));
+        assertThat(flat.hasNext(), is(false));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenEmpty() {
+        Iterator<Iterator<Object>> data = List.of(
+                List.of().iterator()
+        ).iterator();
+        FlatMap<Object> flat = new FlatMap<>(data);
+        flat.next();
+    }
+}
