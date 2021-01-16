22536e43057c4f1ed78b73eca8f04765f9ac5be5
ViktorJava <gipsyscrew@gmail.com>
2021-01-16 20:13:15 +0200

    Generics. #14
    Создал пакет generics: Структуры данных и алгоритмы.

--- chapter_001/src/main/java/ru/job4j/generics/Generics.java
+++ chapter_001/src/main/java/ru/job4j/generics/Generics.java
@@ -1,4 +1,4 @@
-package generics;
+package ru.job4j.generics;
 
 import java.util.ArrayList;
 import java.util.Iterator;
---
+++ chapter_001/src/main/java/ru/job4j/generics/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * 2 Уровень - Junior. Блок 1. Структуры данных и алгоритмы.
+ * Параметризованные типы- Generics.
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 16.01.2021
+ */
+package ru.job4j.generics;
