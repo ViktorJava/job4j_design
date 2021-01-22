d70a48e0a9b07d4efbea8539cfa30948b25c5589
ViktorJava <gipsyscrew@gmail.com>
2021-01-22 23:28:54 +0200

    5.2.1. Реализовать SimpleArray<T> [#438902] #15
    Создана структуру проекта.

---
+++ chapter_001/src/test/java/ru/job4j/generics/SimpleArrayTest.java
@@ -0,0 +1,17 @@
+package ru.job4j.generics;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Тесты универсальной обертки над массивом.
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 22.01.2021
+ */
+public class SimpleArrayTest {
+
+}
