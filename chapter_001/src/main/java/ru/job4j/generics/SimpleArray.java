b27c397442aebc357d6d25f9d1064221cca3ba40
ViktorJava <gipsyscrew@gmail.com>
2021-01-24 14:23:51 +0200

    5.2.1. Реализовать SimpleArray<T> [#438902] #15
    Production implementation.

--- chapter_001/src/main/java/ru/job4j/generics/SimpleArray.java
+++ chapter_001/src/main/java/ru/job4j/generics/SimpleArray.java
@@ -1,36 +1,44 @@
 package ru.job4j.generics;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static java.util.Objects.checkIndex;
 
 /**
  * <h2>5.2.1. Реализовать SimpleArray [#438902]</h2>
- * В этом задании необходимо сделать универсальную обертку над массивом.
+ * Необходимо сделать универсальную обертку над массивом.
  *
  * <p> Добавить методы:
  * <ul>
- * <li>{@code add(T model)} - добавляет указанный элемент (model) в первую свободную ячейку;
- * <li>{@code set(int index, T model)} - заменяет указанным элементом (model) элемент, находящийся по индексу index;
- * <li>{@code remove(int index)} - удаляет элемент по указанному индексу, все находящиеся справа элементы при этом
- * необходимо сдвинуть на единицу влево (в середине массива не должно быть пустых ячеек);
- * <li>{@code get(int index)} - возвращает элемент, расположенный по указанному индексу;
+ * <li>{@code add(T model)} - добавляет указанный элемент (model)
+ * в первую свободную ячейку;
+ * <li>{@code set(int index, T model)} - заменяет указанным элементом (model)
+ * элемент, находящийся по индексу index;
+ * <li>{@code remove(int index)} - удаляет элемент по указанному индексу,
+ * все находящиеся справа элементы при этом необходимо сдвинуть на единицу
+ * влево (в середине массива не должно быть пустых ячеек);
+ * <li>{@code get(int index)} - возвращает элемент, расположенный
+ * по указанному индексу;
  * </ul>
- * <p>Также, реализуйте интерфейс Iterable<T> - метод {@code iterator()} возвращает итератор,
- * предназначенный для обхода данной структуры.
- *
- * <p>Объект должен принимать количество ячеек. Структура не должна быть динамической.
+ * <p>Также, реализовать интерфейс Iterable.<T>
+ * Метод {@code iterator()} возвращает итератор, предназначенный
+ * для обхода данной структуры.
  *
  * @author ViktorJava (gipsyscrew@gmail.com)
  * @version 0.1
- * @implNote 1. В методах, где используется индекс нужно делать валидацию.
+ * @implNote Объект должен принимать количество ячеек. Структура не должна
+ * быть динамической.
+ * 1. В методах, где используется индекс нужно делать валидацию.
  * Индекс должен находиться в рамках добавленных элементов.
- * Например, у вас есть хранилище из 10 элементов. Вы добавили 3 элемента.
- * Каким может быть индекс? [0, 2]. Для проверки индекса используйте метод Objects.checkIndex.
- *
- * <p>2. Для удаления использовать {@code System.arraycopy()} вместо цикла.
+ * Например, есть хранилище из 10 элементов. Добавляем 3 элемента.
+ * Каким может быть индекс? [0, 2]. Для проверки индекса используем метод
+ * {@code Objects.checkIndex()}.
+ * 2. Для удаления использовать {@code System.arraycopy()} вместо цикла.
  * @since 22.01.2021
  */
 public class SimpleArray<T> implements Iterable<T> {
-    private Object[] elements;
+    private final Object[] elements;
     private int pos = 0;
 
     public SimpleArray(int size) {
@@ -41,31 +49,24 @@ public class SimpleArray<T> implements Iterable<T> {
      * Метод добавляет указанный элемент (model) в первую свободную ячейку;
      *
      * @param model Добавляемый элемент.
+     * @throws IndexOutOfBoundsException Переполнение структуры.
      */
-    public void add(T model) {
-        if (pos > elements.length) {
-            throw new IndexOutOfBoundsException();
-        }
+    public void add(T model) throws IndexOutOfBoundsException {
+        checkIndex(pos, elements.length);
         elements[pos++] = model;
     }
 
     /**
-     * Метод заменяет указанным элементом (model) элемент, находящийся по индексу index;
+     * Метод заменяет указанным элементом (model) элемент,
+     * находящийся по индексу index;
      *
      * @param index Индекс заменяемого элемента.
      * @param model Новый элемент.
+     * @throws IndexOutOfBoundsException Выход индекса за пределы структуры.
      */
-    public void set(int index, T model) {
-
-    }
-
-    /**
-     * Метод удаляет элемент по указанному индексу;
-     *
-     * @param index Индекс удаляемого элемента.
-     */
-    public void remove(int index) {
-
+    public void set(int index, T model) throws IndexOutOfBoundsException {
+        checkIndex(index, elements.length);
+        elements[index] = model;
     }
 
     /**
@@ -73,13 +74,44 @@ public class SimpleArray<T> implements Iterable<T> {
      *
      * @param index Индекс возвращаемого элемента.
      * @return Возвращаемый элемент.
+     * @throws IndexOutOfBoundsException Выход индекса за пределы структуры.
      */
-    public T get(int index) {
+
+    public T get(int index) throws IndexOutOfBoundsException {
+        checkIndex(index, elements.length);
         return (T) elements[index];
+    }
+
+    /**
+     * Метод удаляет элемент по указанному индексу;
+     *
+     * @param index Индекс удаляемого элемента.
+     * @throws IndexOutOfBoundsException Выход индекса за пределы структуры.
+     */
+    public void remove(int index) throws IndexOutOfBoundsException {
+        checkIndex(index, elements.length);
+        System.arraycopy(elements, index + 1, elements, index,
+                elements.length - index - 1);
+        elements[--this.pos] = null;
     }
 
     @Override
     public Iterator<T> iterator() {
-        return null;
+        return new Iterator<>() {
+            int counter = 0;
+
+            @Override
+            public boolean hasNext() {
+                return counter < pos;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return (T) elements[counter++];
+            }
+        };
     }
 }
--- chapter_001/src/test/java/ru/job4j/generics/SimpleArrayTest.java
+++ chapter_001/src/test/java/ru/job4j/generics/SimpleArrayTest.java
@@ -2,8 +2,12 @@ package ru.job4j.generics;
 
 import org.junit.Test;
 
-import static org.hamcrest.core.Is.is;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 /**
  * Тесты универсальной обертки над массивом.
@@ -13,14 +17,92 @@ import static org.hamcrest.MatcherAssert.assertThat;
  * @since 22.01.2021
  */
 public class SimpleArrayTest {
+
+    /**
+     * Добавление элементов, в рамках размера структуры.
+     */
     @Test
-    public void addElement() {
-        SimpleArray<Integer> simpleArray = new SimpleArray<>(3);
-        simpleArray.add(1);
-        simpleArray.add(2);
-        simpleArray.add(3);
-        assertThat(simpleArray.get(0), is(1));
-        assertThat(simpleArray.get(1), is(2));
-        assertThat(simpleArray.get(2), is(3));
+    public void whenAddElement() {
+        SimpleArray<String> simpleArray = new SimpleArray<>(3);
+        simpleArray.add("one");
+        simpleArray.add("two");
+        simpleArray.add("three");
+        assertThat(simpleArray.get(0), is("one"));
+        assertThat(simpleArray.get(1), is("two"));
+        assertThat(simpleArray.get(2), is("three"));
+    }
+
+    /**
+     * Тест структуры на отсутствие динамичности и выброс
+     * исключения: выход за пределы массива.
+     */
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void whenIndexOfBounds() {
+        SimpleArray<String> simpleArray = new SimpleArray<>(2);
+        simpleArray.add("1");
+        simpleArray.add("2");
+        simpleArray.add("3");
+    }
+
+    /**
+     * Тест изменения элемента.
+     */
+    @Test
+    public void whenSetElement() {
+        SimpleArray<String> simpleArray = new SimpleArray<>(3);
+        simpleArray.add("one");
+        simpleArray.add("two");
+        simpleArray.add("four");
+        simpleArray.set(2, "three");
+        assertThat(simpleArray.get(0), is("one"));
+        assertThat(simpleArray.get(1), is("two"));
+        assertThat(simpleArray.get(2), is("three"));
+    }
+
+    /**
+     * Тест удаления элемента.
+     */
+    @Test
+    public void whenDelElement() {
+        SimpleArray<String> simpleArray = new SimpleArray<>(3);
+        simpleArray.add("one");
+        simpleArray.add("two");
+        simpleArray.add("three");
+        simpleArray.remove(2);
+        assertThat(simpleArray.get(0), is("one"));
+        assertThat(simpleArray.get(1), is("two"));
+        assertThat(simpleArray.get(2), nullValue());
+        simpleArray.add("three");
+        assertThat(simpleArray.get(2), is("three"));
+    }
+
+    /**
+     * Комплексное тестирование итератора.
+     */
+    @Test
+    public void complexIteratorTest() {
+        SimpleArray<String> simpleArray = new SimpleArray<>(3);
+        Iterator<String> iterator = simpleArray.iterator();
+        simpleArray.add("one");
+        simpleArray.add("two");
+        simpleArray.add("three");
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is("one"));
+        assertThat(iterator.next(), is("two"));
+        assertThat(iterator.next(), is("three"));
+        assertThat(iterator.hasNext(), is(false));
+    }
+
+    /**
+     * Тест итератора с выбрасыванием исключения.
+     */
+    @Test(expected = NoSuchElementException.class)
+    public void whenIteratorExeption() {
+        SimpleArray<String> simpleArray = new SimpleArray<>(1);
+        Iterator<String> iterator = simpleArray.iterator();
+        simpleArray.add("one");
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is("one"));
+        assertThat(iterator.next(), is("two"));
     }
 }
