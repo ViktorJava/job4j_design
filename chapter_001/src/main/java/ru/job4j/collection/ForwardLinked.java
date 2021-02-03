21587579291dc11ab21925174a1dec4810fe8673
ViktorJava <gipsyscrew@gmail.com>
2021-02-03 23:39:10 +0200

    Удалить head в односвязном списке. [#438878] #20
    Реализован метод deleteFirst()

---
+++ chapter_001/src/main/java/ru/job4j/collection/ForwardLinked.java
@@ -0,0 +1,96 @@
+package ru.job4j.collection;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * <h2>Удалить head в односвязном списке. [#438878]</h2>
+ * Необходимо реализовать метод delete для односвязного списка.
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @implNote Реализовать метод {@code deleteFirst()}.
+ * В методе {@code delete} должна быть проверка, что {@code head != null}.
+ * Этот случай проверяется в тесте.
+ * @since 03.02.2021
+ */
+public class ForwardLinked<T> implements Iterable<T> {
+    private Node<T> head;
+
+    /**
+     * Метод добавляет элемент данных в конец списка.
+     *
+     * @param value Добавляемый элемент в хранилище.
+     */
+    public void add(T value) {
+        Node<T> node = new Node<>(value, null);
+        if (head == null) {
+            head = node;
+            return;
+        }
+        Node<T> tail = head;
+        while (tail.next != null) {
+            tail = tail.next;
+        }
+        tail.next = node;
+    }
+
+    /**
+     * Удаление первого элемента в цепочке данных.
+     * По другому, это называется, обнуление ссылки на следующий узел.
+     *
+     * @return Удалённый элемент
+     * @throws NoSuchElementException Исключение бросается при попытке
+     *                                удалить несуществующий элемент.
+     */
+    public T deleteFirst() {
+        T result;
+        if (head != null) {
+            result = head.value;
+            head = head.next;
+        } else {
+            throw new NoSuchElementException();
+        }
+        return result;
+    }
+
+    private static class Node<T> {
+        T value;
+        Node<T> next;
+
+        public Node(T value, Node<T> next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    /**
+     * С целью прикручивания структуры данных к циклу типа forEach,
+     * реализуется интерфейс {@link Iterable}.
+     * Возвращается итератор бегущий по элементам данных, типа {@code <E>}.
+     *
+     * @return Возвращаемый итератор.
+     * @throws NoSuchElementException обращение у несуществующему элементу.
+     */
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<>() {
+            Node<T> node = head;
+
+            @Override
+            public boolean hasNext() {
+                return node != null;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                T value = node.value;
+                node = node.next;
+                return value;
+            }
+        };
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/collection/ForwardLinkedTest.java
@@ -0,0 +1,42 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+/**
+ * Тесты метода удаления первого элемента в цепочке данных.
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 03.02.2021
+ */
+public class ForwardLinkedTest {
+    @Test(expected = NoSuchElementException.class)
+    public void whenDeleteFirst() {
+        ForwardLinked<Integer> linked = new ForwardLinked<>();
+        linked.add(1);
+        linked.deleteFirst();
+        linked.iterator().next();
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenDeleteEmptyLinked() {
+        ForwardLinked<Integer> linked = new ForwardLinked<>();
+        linked.deleteFirst();
+    }
+
+    @Test
+    public void whenMultiDelete() {
+        ForwardLinked<Integer> linked = new ForwardLinked<>();
+        linked.add(1);
+        linked.add(2);
+        assertThat(linked.deleteFirst(), is(1));
+        Iterator<Integer> it = linked.iterator();
+        assertThat(it.next(), is(2));
+    }
+}
