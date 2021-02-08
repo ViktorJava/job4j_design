f9c2b41691f698610f0a1f02420c9da5105b2c9e
ViktorJava <gipsyscrew@gmail.com>
2021-02-08 22:48:03 +0200

    Используя контейнер на базе связанного списка создать контейнер Stack [#438879] #21
    Добавил метод isEmpty().
    Поправил JavaDoc.

--- chapter_001/src/main/java/ru/job4j/collection/SimpleStack.java
+++ chapter_001/src/main/java/ru/job4j/collection/SimpleStack.java
@@ -1,15 +1,21 @@
 package ru.job4j.collection;
 
 /**
+ * <h2>Используя контейнер на базе связанного списка создать контейнер Stack [#438879]</h2>
+ * <li>{@code Stack} - примитивная структура данных.
+ * <li>{@code Stack} лучше всего реализовать на базе связанного списка.
+ * <li>Связанный список умеет быстро вставлять данные и удалять.
+ * <li>Это как раз те действия, которые нужны для {@code Stack}.
+ *
  * @author ViktorJava (gipsyscrew@gmail.com)
- * @version 0.1
+ * @version 0.2
  * @since 04.02.2021
  */
 public class SimpleStack<T> {
-    private ForwardLinked<T> forwardLinked = new ForwardLinked<>();
+    private final ForwardLinked<T> forwardLinked = new ForwardLinked<>();
 
     /**
-     * Метод удаляет элемент из коллекции и возвращает его значение.
+     * Метод удаляет элемент из стека и возвращает его значение.
      *
      * @return Возвращаемое значение.
      */
@@ -18,11 +24,20 @@ public class SimpleStack<T> {
     }
 
     /**
-     * Метод помещает значение в коллекцию.
+     * Метод помещает значение в стек.
      *
-     * @param value Значение помещаемое в коллекцию.
+     * @param value Значение помещаемое в стек.
      */
     public void push(T value) {
         forwardLinked.add(value);
+    }
+
+    /**
+     * Метод проверяет стек на пустоту.
+     *
+     * @return true если стек пустой, иначе false.
+     */
+    public boolean isEmpty() {
+        return forwardLinked.isEmpty();
     }
 }
