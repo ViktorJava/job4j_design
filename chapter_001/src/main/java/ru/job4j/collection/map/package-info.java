6e5a7b67e330805b105d94db225bae3b3dcb1811
ViktorJava <gipsyscrew@gmail.com>
2021-02-14 20:17:52 +0200

    написал package-info для модулей map и iterator.

---
+++ chapter_001/src/main/java/ru/job4j/collection/map/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Модуль с различными реализациями интерфейса Map.
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 14.02.2021
+ */
+package ru.job4j.collection.map;
---
+++ chapter_001/src/main/java/ru/job4j/iterator/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Итератор во всех его проявлениях.
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 14.02.2021
+ */
+package ru.job4j.iterator;
