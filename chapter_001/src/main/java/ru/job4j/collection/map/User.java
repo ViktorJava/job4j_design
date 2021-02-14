04bc063726309267a31426e825041ec23e394364
ViktorJava <gipsyscrew@gmail.com>
2021-02-14 20:24:04 +0200

    Создать модель User. #27

---
+++ chapter_001/src/main/java/ru/job4j/collection/map/User.java
@@ -0,0 +1,14 @@
+package ru.job4j.collection.map;
+
+import java.util.Calendar;
+
+/**
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 14.02.2021
+ */
+public class User {
+    String name;
+    int children;
+    Calendar birthday;
+}
