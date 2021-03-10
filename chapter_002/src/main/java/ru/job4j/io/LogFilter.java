dceb4807fb4dee3da73cf3ffa1b8c0f628db873b
ViktorJava <gipsyscrew@gmail.com>
2021-03-10 22:55:25 +0200

    FileInput #35
    Написал два разных метода чтения файлов.

---
+++ chapter_002/src/main/java/ru/job4j/io/LogFilter.java
@@ -0,0 +1,20 @@
+package ru.job4j.io;
+
+import java.util.List;
+
+/**
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 10.03.2021
+ */
+public class LogFilter {
+    public static List<String> filter(String file) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        List<String> log = filter("log.txt");
+        System.out.println(log);
+    }
+}
