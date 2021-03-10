1e07e595b0cbb2ec3af14489027ca64607d4eca1
ViktorJava <gipsyscrew@gmail.com>
2021-03-11 01:55:49 +0200

    BufferedReader #36
    Написал метод filter()

--- chapter_002/src/main/java/ru/job4j/io/LogFilter.java
+++ chapter_002/src/main/java/ru/job4j/io/LogFilter.java
@@ -1,20 +1,42 @@
 package ru.job4j.io;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * Фильтрация лог файла.
  *
  * @author ViktorJava (gipsyscrew@gmail.com)
  * @version 0.1
  * @since 10.03.2021
  */
 public class LogFilter {
+    /**
+     * Метод применяет буферизированный поток, фильтрует лог файл.
+     *
+     * @param file Имя лог файла.
+     * @return Список отфильтрованных строк.
+     */
     public static List<String> filter(String file) {
-        return null;
+        List<String> result = new ArrayList<>();
+        List<String> temp = new ArrayList<>();
+        try (BufferedReader in = new BufferedReader(new FileReader(file))) {
+            in.lines().forEach(temp::add);
+            for (String s: temp) {
+                if (s.contains("404")) {
+                    result.add(s);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 
     public static void main(String[] args) {
         List<String> log = filter("log.txt");
-        System.out.println(log);
+        System.out.print(log);
     }
 }
