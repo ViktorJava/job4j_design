889aef51fe10e374ca577340217ed48981a04ca5
ViktorJava <gipsyscrew@gmail.com>
2021-03-12 22:24:13 +0200

    Читаем файл конфигурации #38
    Написал шаблон класса и тестов.

---
+++ chapter_002/src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.io;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+/**
+ * Тесты чтения файла конфигурации.
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 12.03.2021
+ */
+public class ConfigTest {
+    @Test
+    public void whenPairWithoutComment() {
+        String path = "./data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(
+                config.value("name"),
+                is("Petr Arsentev")
+        );
+    }
+}
