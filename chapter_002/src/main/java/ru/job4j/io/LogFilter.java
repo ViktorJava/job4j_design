6e11ce2dc49c537110c2eef0a800e56409231f07
ViktorJava <gipsyscrew@gmail.com>
2021-03-12 07:36:15 +0200

    BufferedOutputStream #37
    Написал метод save().

--- chapter_002/src/main/java/ru/job4j/io/LogFilter.java
+++ chapter_002/src/main/java/ru/job4j/io/LogFilter.java
@@ -1,7 +1,6 @@
 package ru.job4j.io;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,8 +30,25 @@ public class LogFilter {
         return result;
     }
 
+    /**
+     * Метод получает данные лог файла на вход и записывает в файл.
+     *
+     * @param log  Записываемые данные.
+     * @param file Имя файла.
+     */
+    public static void save(List<String> log, String file) {
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(new FileOutputStream(file)))) {
+            for (String s: log) {
+                out.println(s);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         List<String> log = filter("log.txt");
-        System.out.print(log);
+        save(log, "404.txt");
     }
 }
