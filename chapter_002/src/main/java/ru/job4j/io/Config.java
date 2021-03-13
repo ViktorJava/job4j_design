8143e5dcea7c272babfb1b5e24cb162b64a1e9cb
ViktorJava <gipsyscrew@gmail.com>
2021-03-13 13:19:32 +0200

    Читаем файл конфигурации #38
    Реализовал методы load(), value().

--- chapter_002/src/main/java/ru/job4j/io/Config.java
+++ chapter_002/src/main/java/ru/job4j/io/Config.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.StringJoiner;
 
 /**
  * <h2>Чтение файла конфигурации.</h2>
@@ -15,31 +14,37 @@ import java.util.StringJoiner;
  */
 public class Config {
     private final String path;
-    private final Map<String, String> values = new HashMap<String, String>();
+    private final Map<String, String> values = new HashMap<>();
 
     public Config(final String path) {
         this.path = path;
     }
 
+    /**
+     * Метод должен считать все ключи и их значения в карту values.
+     * В файле могут быть пустые строки и комментарии их пропускаем.
+     */
     public void load() {
-    }
-
-    public String value(String key) {
-        throw new UnsupportedOperationException("Don't impl this method yet!");
-    }
-
-    @Override
-    public String toString() {
-        StringJoiner out = new StringJoiner(System.lineSeparator());
         try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
-            read.lines().forEach(out::add);
+            read.lines()
+                .filter(e -> !e.contains("#") && e.contains("="))
+                .map(e -> e.split("="))
+                .forEach(e -> values.put(e[0], e[1]));
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return out.toString();
     }
 
-    public static void main(String[] args) {
-        System.out.println(new Config("app.properties"));
+    /**
+     * Метод возвращает значение ключа.
+     *
+     * @param key Ключ.
+     * @return Значение ключа.
+     */
+    public String value(String key) {
+        if (!values.containsKey(key)) {
+            throw new UnsupportedOperationException("Don't impl this method yet!");
+        }
+        return values.get(key);
     }
 }
