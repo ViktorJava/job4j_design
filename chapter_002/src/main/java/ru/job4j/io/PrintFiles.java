8a83f4992708a4e7612443daea1913fcb25bc8bf
ViktorJava <gipsyscrew@gmail.com>
2021-03-23 23:02:32 +0200

    Сканирование файловой системы #41
    Написал класс PrintFiles.

--- chapter_002/src/main/java/ru/job4j/io/PrintFiles.java
+++ chapter_002/src/main/java/ru/job4j/io/PrintFiles.java
@@ -5,31 +5,54 @@ import java.nio.file.FileVisitResult;
 import java.nio.file.FileVisitor;
 import java.nio.file.Path;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+import static java.nio.file.FileVisitResult.CONTINUE;
 
 /**
+ * Это специальный класс, в котором инкапсулируется вся логика обхода дерева файлов.
+ * Поместим сюда всю логику поиска файлов по определённому предикату.
+ *
  * @author ViktorJava (gipsyscrew@gmail.com)
  * @version 0.1
  * @since 18.03.2021
  */
 public class PrintFiles implements FileVisitor<Path> {
+    List<Path> paths = new ArrayList<>();
+    Predicate<Path> condition;
+
+    public PrintFiles(Predicate<Path> condition) {
+        this.paths = new ArrayList<>();
+        this.condition = condition;
+    }
+
+    public List<Path> getPaths() {
+        return paths;
+    }
+
     @Override
+
     public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
-        return FileVisitResult.CONTINUE;
+        return CONTINUE;
     }
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        System.out.println(file.toAbsolutePath());
-        return FileVisitResult.CONTINUE;
+        if (condition.test(file)) {
+            paths.add(file);
+        }
+        return CONTINUE;
     }
 
     @Override
     public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
-        return FileVisitResult.CONTINUE;
+        return CONTINUE;
     }
 
     @Override
     public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
-        return FileVisitResult.CONTINUE;
+        return CONTINUE;
     }
 }
--- chapter_002/src/main/java/ru/job4j/io/Search.java
+++ chapter_002/src/main/java/ru/job4j/io/Search.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.function.Predicate;
 
 /**
  * <h2>Сканирование файловой системы.</h2>
@@ -16,6 +18,12 @@ public class Search {
 //TODO пока даже шаблон написать не могу.
     public static void main(String[] args) throws IOException {
         Path start = Paths.get(".");
-        Files.walkFileTree(start, new PrintFiles());
+        //Files.walkFileTree(start, new PrintFiles());
+        search(start, p -> p.toFile().getName().endsWith("java")).forEach(System.out::println);
+    }
+    public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
+        PrintFiles searcher = new PrintFiles(condition);
+        Files.walkFileTree(root, searcher);
+        return searcher.getPaths();
     }
 }
