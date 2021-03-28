a17eae7283cb9a07f865b68f3b6c7cab49944c35
ViktorJava <gipsyscrew@gmail.com>
2021-03-29 00:33:04 +0300

    Архивировать проект #45
    Мелкие правки, javaDoc.

--- chapter_002/src/main/java/ru/job4j/io/Zip.java
+++ chapter_002/src/main/java/ru/job4j/io/Zip.java
@@ -9,16 +9,28 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 /**
+ * <h2>Архивировать проект</h2>
+ * Класс, представляет собой, утилиту для архивации папки.
+ *
  * @author ViktorJava (gipsyscrew@gmail.com)
  * @version 0.1
  * @since 27.03.2021
  */
 public class Zip {
+    /**
+     * Метод архивирует файлы, путь и имя которых получаем через аргумент
+     * в виде списка.
+     *
+     * @param sources Список архивируемых файлов.
+     * @param target  Имя и расширение архивного файла.
+     */
     public void packFiles(List<File> sources, File target) {
-        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+        try (ZipOutputStream zip = new ZipOutputStream(
+                new BufferedOutputStream(new FileOutputStream(target)))) {
             for (File file: sources) {
                 zip.putNextEntry(new ZipEntry(file.getPath()));
-                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(file))) {
+                try (BufferedInputStream out = new BufferedInputStream(
+                        new FileInputStream(file))) {
                     zip.write(out.readAllBytes());
                 }
             }
@@ -29,18 +41,17 @@ public class Zip {
 
     public static void main(String[] args) {
         ArgsName argsName = ArgsName.of(args);
-        Path root = Paths.get(argsName.get("d"));
+        Path root = Paths.get(argsName.get("d")); //directory
         try {
-            List<Path> listPath = Search.search(root, p -> !p
-                    .toFile()
-                    .getName()
-                    .endsWith(argsName.get("e")));
+            List<Path> listPath = Search.search(root,
+                    p -> !p.toFile()
+                           .getName()
+                           .endsWith(argsName.get("e"))); //exclude
             List<File> listFile = new ArrayList<>();
             for (Path path: listPath) {
                 listFile.add(path.toFile());
-                System.out.println(path.toFile());
             }
-            new Zip().packFiles(listFile, new File(argsName.get("o")));
+            new Zip().packFiles(listFile, new File(argsName.get("o"))); //output
         } catch (Exception e) {
             e.printStackTrace();
         }
