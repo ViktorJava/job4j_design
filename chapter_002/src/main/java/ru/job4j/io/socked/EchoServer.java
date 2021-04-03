b270b8d4784bdc75bcb5ad04e91dda95a309ffe8
ViktorJava <gipsyscrew@gmail.com>
2021-04-03 08:12:32 +0300

    Что такое Socket? #48
    Done.

---
+++ chapter_002/src/main/java/ru/job4j/io/socked/EchoServer.java
@@ -0,0 +1,36 @@
+package ru.job4j.io.socked;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Класс реализации simple server, работающего по протоколу HTTP.
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 01.04.2021
+ */
+public class EchoServer {
+    public static void main(String[] args) throws IOException {
+        try (ServerSocket server = new ServerSocket(9000)) {
+            while (!server.isClosed()) {
+                Socket socket = server.accept();
+                try (OutputStream out = socket.getOutputStream();
+                     BufferedReader in = new BufferedReader(
+                             new InputStreamReader(socket.getInputStream()))) {
+                    String str;
+                    while (!(str = in.readLine()).isEmpty()) {
+                        if (str.contains("Bye")) {
+                            out.write("HTTP/1.1 200 OK\r\n".getBytes());
+                            server.close();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
