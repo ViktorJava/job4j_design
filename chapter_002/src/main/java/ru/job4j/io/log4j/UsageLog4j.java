30bd80d2f3d7180fab68ee34e8624e3f1e5a44ec
ViktorJava <gipsyscrew@gmail.com>
2021-04-04 22:16:04 +0300

    Simple Login Facade 4 Java #51
    Подключил к проекту библиотеку slf4j.
    Поправил импорты и создание логгера.

--- chapter_002/src/main/java/ru/job4j/io/log4j/UsageLog4j.java
+++ chapter_002/src/main/java/ru/job4j/io/log4j/UsageLog4j.java
@@ -1,16 +1,18 @@
 package ru.job4j.io.log4j;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
+ * Пример класса, работы с библиотекой логирования slf4j.
  *
  * @author ViktorJava (gipsyscrew@gmail.com)
  * @version 0.1
  * @since 04.04.2021
  */
 public class UsageLog4j {
-    private static final Logger LOG = LogManager.getLogger(UsageLog4j.class.getName());
+    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
         LOG.trace("trace message");
