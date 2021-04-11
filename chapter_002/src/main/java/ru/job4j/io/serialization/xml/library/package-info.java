9c6a98b73a2b2ba84d37d3f6efc4165de399174c
ViktorJava <gipsyscrew@gmail.com>
2021-04-11 19:05:52 +0300

    Формат XML #56
    Удалил пакет library и его классы.

--- chapter_002/src/main/java/ru/job4j/io/serialization/xml/library/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * Пакет для классов книг и библиотек.
- * Классы применяются для тренировки навыков написания XML.
- *
- * @author ViktorJava (gipsyscrew@gmail.com)
- * @version 0.1
- * @since 11.04.2021
- */
-package ru.job4j.io.serialization.xml.library;
