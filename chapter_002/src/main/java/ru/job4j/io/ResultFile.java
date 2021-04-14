ac5713ba3aef2eedc9da7ed11d54ec86691b37ec
ViktorJava <gipsyscrew@gmail.com>
2021-04-15 01:12:58 +0300

    FileOutputStream #33
    Сменил путь сохранения файла с таблицей умножения ../chapter_002/data/multiple.txt

--- chapter_002/src/main/java/ru/job4j/io/ResultFile.java
+++ chapter_002/src/main/java/ru/job4j/io/ResultFile.java
@@ -19,7 +19,7 @@ public class ResultFile {
      */
     public int[][] multiple(int size) {
         int[][] table = new int[size][size];
-        try (FileOutputStream out = new FileOutputStream("multiple.txt")) {
+        try (FileOutputStream out = new FileOutputStream("../chapter_002/data/multiple.txt")) {
             for (int i = 0; i < size; i++) {
                 out.write("\n".getBytes());
                 for (int j = 0; j < size; j++) {
