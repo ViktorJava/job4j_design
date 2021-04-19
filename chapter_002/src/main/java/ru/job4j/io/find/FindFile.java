b44a53fd67c0fa5235629de7871a56dec010af5f
ViktorJava <gipsyscrew@gmail.com>
2021-04-19 21:19:15 +0300

    Поиск файлов по критерию #60

---
+++ chapter_002/src/main/java/ru/job4j/io/find/FindFile.java
@@ -0,0 +1,14 @@
+package ru.job4j.io.find;
+
+/**
+ * <h2>Поиск файлов по критерию.</h2>
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 14.04.2021
+ */
+public class FindFile {
+    public static void main(String... args) {
+
+    }
+}
