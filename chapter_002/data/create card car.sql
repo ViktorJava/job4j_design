d7805e3abd0cf62d4ab2c851e4f377d765b948da
ViktorJava <gipsyscrew@gmail.com>
2021-04-25 01:13:42 +0300

    Что такое база данных #61
    поправил обновление записи в таблице.

--- chapter_002/data/create card car.sql
+++ chapter_002/data/create card car.sql
@@ -8,9 +8,7 @@ id serial primary key,
 	price int
 );
 insert into car_card(car_model, date_creation, price) values('BMW', '22/08/2014', 25000);
-update car_card set car_model='Mercedes';
-update car_card set price='19000';
+update car_card set car_model='Mercedes', price='19000';
 select * from car_card;
 -- Очистка таблицы.
 delete from car_card;
-
