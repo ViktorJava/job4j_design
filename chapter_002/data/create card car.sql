e1aac4cf043a3bb234f20d4b7411e5b84d38f49c
ViktorJava <gipsyscrew@gmail.com>
2021-04-25 00:42:04 +0300

    Что такое база данных #61
    Написал SQL скрипт.
    
    название базы данных: shop
    название таблицы: car_card
    car_model varchar(30)
    date_creation date
    price int

---
+++ chapter_002/data/create card car.sql
@@ -0,0 +1,16 @@
+-- Удаление таблицы если существует.
+DROP TABLE IF EXISTS car_card;
+
+create table car_card(
+id serial primary key,
+	car_model varchar (30),
+	date_creation date,
+	price int
+);
+insert into car_card(car_model, date_creation, price) values('BMW', '22/08/2014', 25000);
+update car_card set car_model='Mercedes';
+update car_card set price='19000';
+select * from car_card;
+-- Очистка таблицы.
+delete from car_card;
+
