a1e6d86b52a3f4a3ad18f062ad750236dcefa69f
ViktorJava <gipsyscrew@gmail.com>
2021-04-26 23:45:15 +0300

    Поиск файлов по критерию #60
    Пишу, ещё много писать...

---
+++ chapter_002/src/main/java/ru/job4j/io/find/ArgsParser.java
@@ -0,0 +1,61 @@
+package ru.job4j.io.find;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * <h2>Именованные аргументы</h2>
+ * Класс реализует приём массива параметров и разбивание их на пары.
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 25.04.2021
+ */
+public class ArgsParser {
+    private String directory;
+    private String fileName;
+    private String type;
+    private String outputName;
+
+    private final Map<String, String> values = new HashMap<>();
+
+    /**
+     * Метод возвращает значение ключа.
+     *
+     * @param key Ключ.
+     * @return Значение
+     */
+    public String get(String key) {
+        if (!values.containsKey(key)) {
+            throw new IllegalArgumentException("No such parameter exists: " + key);
+        }
+        return values.get(key);
+    }
+
+    /**
+     * Приватный метод, парсит массив параметров полученный в виде аргумента.
+     *
+     * @param args Массив параметров в формате -ключ=значение.
+     */
+    private void parse(String[] args) {
+        for (String arg: args) {
+            String[] split = arg.replaceFirst("-", "").split("=");
+            if (split.length != 2) {
+                throw new IllegalArgumentException("argument must be: -param=value");
+            }
+            values.put(split[0], split[1]);
+        }
+    }
+
+    /**
+     * Фабричный метод.
+     *
+     * @param args Массив параметров в формате -ключ=значение.
+     * @return Объект типа ArgsName с распарсенными параметрами.
+     */
+    public static ArgsParser of(String[] args) {
+        ArgsParser names = new ArgsParser();
+        names.parse(args);
+        return names;
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/io/find/Finder.java
@@ -0,0 +1,44 @@
+package ru.job4j.io.find;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.job4j.io.log4j.UsageLog4j;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.function.Predicate;
+
+/**
+ * <h2>Поиск файлов по критерию.</h2>
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 14.04.2021
+ */
+public class Finder {
+    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
+
+    public static void main(String... args) {
+        //try {
+            ArgsParser argsParser = ArgsParser.of(args);
+        System.out.println(argsParser.get("d"));
+        System.out.println(argsParser.get("n"));
+        System.out.println(argsParser.get("t"));
+        System.out.println(argsParser.get("o"));
+            //Predicate<Path> condition = new Predicate<Path>()
+
+
+            //List<Path> result = search(start, condition);
+        //} catch (IOException e) {
+        //    LOG.error(e.getMessage(), e);
+        //}
+    }
+
+    public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
+        Search searcher = new Search(condition);
+        Files.walkFileTree(root, searcher);
+        return searcher.getPaths();
+    }
+}
