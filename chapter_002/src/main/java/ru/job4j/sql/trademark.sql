9af0082573ac7f195e8583fe5d9314386e102742
ViktorJava <gipsyscrew@gmail.com>
2021-04-30 09:27:26 +0300

    Inner join #63
    Исправил опечатку в имени атрибута.

--- chapter_002/src/main/java/ru/job4j/sql/trademark.sql
+++ chapter_002/src/main/java/ru/job4j/sql/trademark.sql
@@ -6,18 +6,18 @@ CREATE TABLE country(
 CREATE TABLE trademark(
 	id serial primary key,
 	name varchar(100),
-	vehicle_id int references country(id)
+	country_id int references country(id)
 );
 insert into country(name) values('Russia');
 insert into country(name) values('Ukraine');
 insert into country(name) values('China');
 
-insert into trademark(name, vehicle_id) values ('Job4j', 1);
-insert into trademark(name, vehicle_id) values ('SoftServe', 2);
-insert into trademark(name, vehicle_id) values ('Huawei', 3);
+insert into trademark(name, country_id) values ('Job4j', 1);
+insert into trademark(name, country_id) values ('SoftServe', 2);
+insert into trademark(name, country_id) values ('Huawei', 3);
 insert into trademark(name) values('Horns and hooves');
 
---select tm.name from trademark tm;
---SELECT * FROM trademark JOIN country co ON trademark.vehicle_id = co.id;
---SELECT tm.name, co.name FROM trademark AS tm JOIN country AS co ON tm.vehicle_id = co.id;
---SELECT tm.name FROM trademark tm WHERE id in (SELECT id FROM country);
+select tm.name from trademark tm;
+SELECT * FROM trademark JOIN country co ON trademark.country_id = co.id;
+SELECT tm.name, co.name FROM trademark AS tm JOIN country AS co ON tm.country_id = co.id;
+SELECT tm.name FROM trademark tm WHERE id in (SELECT id FROM country); 
