165300e31750a3d9db1ffe9b15a4edf0f91a2233
ViktorJava <gipsyscrew@gmail.com>
2021-05-04 20:03:34 +0300

    JDBC #68
    Прикрутил класс Properties для работы с конфиг.файлом.
    Добавил ClassLoader для правильной работы с относительными путями файлов.

--- chapter_003/src/main/java/ru/job4j/jdbc/ConnectionDemo.java
+++ chapter_003/src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -1,9 +1,11 @@
 package ru.job4j.jdbc;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 /**
  * Пример подключения к Postgres db через JDBC.
@@ -13,13 +15,16 @@ import java.sql.SQLException;
  * @since 04.05.2021
  */
 public class ConnectionDemo {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        Config config = new Config("./src/main/resources/app.properties");
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
+        Properties properties = new Properties();
+        //ClassLoader для получения пути к папке resources.
+        ClassLoader classLoader = ConnectionDemo.class.getClassLoader();
+        properties.load(classLoader.getResourceAsStream("app.properties"));
+
         Class.forName("org.postgresql.Driver");
-        config.load();
-        String url = config.value("url");
-        String login = config.value("login");
-        String password = config.value("password");
+        String url = properties.getProperty("url");
+        String login = properties.getProperty("login");
+        String password = properties.getProperty("password");
         try (Connection connection = DriverManager.getConnection(url, login, password)) {
             DatabaseMetaData metaData = connection.getMetaData();
             System.out.println(metaData.getUserName());
