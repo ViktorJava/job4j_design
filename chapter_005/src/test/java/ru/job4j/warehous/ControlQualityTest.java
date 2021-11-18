68674a1efa273d5242ca0f65d2e7322e8f45123b
ViktorJava <gipsyscrew@gmail.com>
2021-11-18 02:27:15 +0200

    Хранилище продуктов #99
    Добавил тесты.

--- chapter_005/src/test/java/ru/job4j/warehous/ControlQualityTest.java
+++ chapter_005/src/test/java/ru/job4j/warehous/ControlQualityTest.java
@@ -1,9 +1,13 @@
 package ru.job4j.warehous;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.time.LocalDate;
 import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 /**
  * @author ViktorJava (gipsyscrew@gmail.com)
@@ -11,17 +15,60 @@ import java.util.List;
  * @since 15.11.2021
  */
 public class ControlQualityTest {
-    @Test
-    public void test() {
-        List<Storage> storages = List.of(
+    /**
+     * Ссылка на список хранилищ.
+     */
+    List<Storage> storageList;
+    /**
+     * Ссылка на объект контроля качества.
+     */
+    ControlQuality controlQuality;
+
+    @Before
+    public void setup() {
+        storageList = List.of(
                 new Shop(),
                 new Trash(),
                 new Warehouse()
         );
-//        Storage trash = new Trash();
-        ControlQuality controlQuality = new ControlQuality(storages);
+    }
 
-        controlQuality.test(new Coffee("Jacobs", LocalDate.now().minusMonths(1), LocalDate.now().minusDays(1), 120, 0));
-        controlQuality.test(new Coffee("Cake", LocalDate.now().minusMonths(1), LocalDate.now(), 120, 0));
+    /**
+     * Свежесть продукта израсходована на 103% = Trash.
+     */
+    @Test
+    public void testTrash() {
+        Food coffee = new Coffee("Jacobs", LocalDate.now().minusMonths(1),
+                LocalDate.now().minusDays(1), 120, 0);
+        controlQuality = new ControlQuality(storageList);
+        controlQuality.test(coffee);
+        List<Food> expected = storageList.get(1).get();
+        assertThat(expected.get(0).getName(), is("Jacobs"));
+    }
+
+    /**
+     * Свежесть продукта израсходована на 96% = Warehouse.
+     */
+    @Test
+    public void testShopAndDiscount() {
+        Food coffee = new Coffee("MacCoffee", LocalDate.now().minusMonths(1),
+                LocalDate.now().plusDays(1), 100, 0);
+        controlQuality = new ControlQuality(storageList);
+        controlQuality.test(coffee);
+        List<Food> expected = storageList.get(0).get();
+        assertThat(expected.get(0).getDiscount(), is(25));
+    }
+
+    /**
+     * Свежесть продукта израсходована на 3% = Warehouse.
+     */
+    @Test
+    public void testWarehouse() {
+        Food coffee = new Coffee("BestOfTheBest", LocalDate.now().minusDays(1),
+                LocalDate.now().plusMonths(1), 300, 0);
+        controlQuality = new ControlQuality(storageList);
+        controlQuality.test(coffee);
+        List<Food> expected = storageList.get(2).get();
+        assertThat(expected.get(0).getName(), is("BestOfTheBest"));
     }
 }
