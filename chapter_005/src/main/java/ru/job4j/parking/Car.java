97fff6528b27ff9dc8733e87130bff6888ae28e2
ViktorJava <gipsyscrew@gmail.com>
2021-11-20 02:56:37 +0200

    Парковка машин #100 этап 1

---
+++ chapter_005/src/main/java/ru/job4j/parking/Car.java
@@ -0,0 +1,17 @@
+package ru.job4j.parking;
+
+/**
+ * Интерфейс описывающий абстрактный автомобиль.
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 20.11.2021
+ */
+public interface Car {
+    /**
+     * Метод возвращает размер автомобиля.
+     *
+     * @return Размер автомобиля.
+     */
+    int getSize();
+}
---
+++ chapter_005/src/main/java/ru/job4j/parking/Parking.java
@@ -0,0 +1,18 @@
+package ru.job4j.parking;
+
+/**
+ * Интерфейс описывающий абстрактную парковку для автомобилей.
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 20.11.2021
+ */
+public interface Parking {
+    /**
+     * Метод принимает автомобиль на парковку.
+     *
+     * @param car Объект типа Car.
+     * @return true- в случае удачной парковки, иначе false.
+     */
+    boolean accept(Car car);
+}
